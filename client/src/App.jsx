d99b7c523ba67af4974e5e26876bc60b1949a62c
Vangelis Trikoupis <vangelis.trikoupis@gmail.com>
2020-10-15 10:43:52 +0200

    clean up, docs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,47 +12,39 @@ const App = () => {
   const [event, setEvent] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false);
   const [initVal, setInitVal] = useState(null)
-  const [changes, setChanges] = useState(null)
   const [intendedCellToEdit, setIntendedCellToEdit] = useState(null)
+  const columns = ['Uid', 'Name', 'Email', 'Modules', 'Details'];
 
-  useEffect(() => { // cell focused state updated
+
+  // 1st state update: cell focused
+  useEffect(() => {
     if (event) {
-      console.log("event captured")
-      console.log(event)
       setIntendedCellToEdit(event.column.dataField)
     }
   }, [event])
 
-  useEffect(() => { // init val has been set
+  // 2nd state update: capture the value of that cell
+  useEffect(() => {
     if (event) {
       console.log("intendedCell captured of column: " + intendedCellToEdit + " and content " + event.data[intendedCellToEdit])
       setInitVal(event.data[intendedCellToEdit])
     }
-
   }, [intendedCellToEdit])
 
   const startingToEdit = (e) => {
     // cell is in focus
     // we can get the values for that row before updating anything
     const { _id, uid, name, role, email, modules, details } = e.data
-    setEvent(e);
-
-    setInitVal(() => e.data[intendedCellToEdit])
-
-    /*  Should the api be updated after each event or should a button "Save changes" write updates in batch?
-        in the first option, should there be a debounce maybe so that we can poll multiple writes, for example every 10 seconds?
+    /*  devextreme events are synchronous but our state is updating asynchronously. Let everything that needs to happen, 
+        happen in the useEffect hooks, with the corresponding dependency array
     */
-
+    setEvent(e);
   }
 
   const rowUpdated = (e) => {
     // cell is in focus
     // we can get the values for that row before updating anything
-    console.log(initVal)
     console.log("row updated")
-    console.log(e.data[intendedCellToEdit])
-
-
     /*  Should the api be updated after each event or should a button "Save changes" write updates in batch?
         in the first option, should there be a debounce maybe so that we can poll multiple writes, for example every 10 seconds?
         if()
@@ -85,8 +77,8 @@ const App = () => {
           showBorders={true}
           onEditingStart={(e) => startingToEdit(e)}
           onRowUpdated={(e) => rowUpdated(e)}
-
-
+        // this next line only works with static data? why not with dynamic?
+        // defaultColumns={columns}
         >
           <Editing
             mode="cell"
